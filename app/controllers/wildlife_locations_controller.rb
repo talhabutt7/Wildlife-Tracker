ce518b54580f5fb93d7aaa9838d0b91ec69d554d
talhabutt7 <talhabuttt@gmail.com>
2025-02-02 15:41:49 +0000

    Create and show all the locations on the homepage

--- app/controllers/wildlife_locations_controller.rb
+++ app/controllers/wildlife_locations_controller.rb
@@ -1,24 +1,28 @@
+# app/controllers/wildlife_locations_controller.rb
 class WildlifeLocationsController < ApplicationController
   def index
+    @wildlife_locations = WildlifeLocation.all
     if params[:latitude] && params[:longitude] && params[:radius]
-      @locations = WildlifeLocation.nearby(params[:latitude].to_f, params[:longitude].to_f, params[:radius].to_i)
+      locations = WildlifeLocation.nearby(params[:latitude].to_f, params[:longitude].to_f, params[:radius].to_i)
     else
-      @locations = WildlifeLocation.all
+      locations = WildlifeLocation.all
     end
-
     respond_to do |format|
       format.turbo_stream
       format.html
-      format.json { render json: @locations }
+      format.json { render json: locations }
     end
   end
 
   def create
-    location = WildlifeLocation.new(location_params)
-    if location.save
-      render turbo_stream: turbo_stream.append("locations", partial: "locations/location", locals: { location: location })
+    @wildlife_location = WildlifeLocation.new(location_params)
+    if @wildlife_location.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to wildlife_locations_path, notice: "Site added." }
+      end
     else
-      render json: location.errors, status: :unprocessable_entity
+      render json: @wildlife_location.errors, status: :unprocessable_entity
     end
   end
 
