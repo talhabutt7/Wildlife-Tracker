a04d0baa6677e0fa96e15fbd10f040ad00f8931d
talhabutt7 <talhabuttt@gmail.com>
2025-02-02 16:02:02 +0000

    Showing marker on the map, New location added on the homepage without reloading the page.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,27 +1,3 @@
-// import { Controller } from "@hotwired/stimulus"
-// import L from "leaflet"
-//
-// export default class extends Controller {
-//   static values = { sites: Array } // Expects JSON data of sites
-//
-//   connect() {
-//     // Initialize Leaflet map
-//     this.map = L.map(this.element).setView([51.505, -0.09], 13)
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map)
-//
-//     // Add markers from data
-//     this.addMarkers()
-//   }
-//
-//   addMarkers() {
-//     this.sitesValue.forEach(site => {
-//       const [lng, lat] = site.coordinates.replace(/SRID=4326;POINT\(|\)/g, '').split(' ')
-//       L.marker([lat, lng]).addTo(this.map).bindPopup(site.name)
-//     })
-//   }
-// }
-
-// app/javascript/controllers/map_controller.js
 import { Controller } from "@hotwired/stimulus"
 import L from "leaflet"
 
@@ -29,6 +5,12 @@ export default class extends Controller {
   connect() {
     this.initializeMap()
     this.loadMarkers()
+      window.addEventListener('marker:added', event => {
+          const { latitude, longitude, name } = event.detail
+          L.marker([parseFloat(latitude), parseFloat(longitude)])
+              .addTo(this.map)
+              .bindPopup(name)
+      })
   }
 
   initializeMap() {
@@ -37,15 +19,16 @@ export default class extends Controller {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map)
   }
 
-  loadMarkers() {
-    fetch('/wildlife_locations.json')
-        .then(response => response.json())
-        .then(data => {
-          data.forEach(site => {
-            L.marker([parseFloat(site.latitude), parseFloat(site.longitude)])
-                .addTo(this.map)
-                .bindPopup(site.name)
-          })
-        })
-  }
+    loadMarkers() {
+        fetch('/wildlife_locations.json')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(site => {
+                    L.marker([parseFloat(site.latitude), parseFloat(site.longitude)])
+                        .addTo(this.map)
+                        .bindPopup(site.name)
+                })
+
+            })
+    }
 }
