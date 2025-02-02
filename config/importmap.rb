79b3e071c1d95509e40a21464590ac1e52060b3d
talhabutt7 <talhabuttt@gmail.com>
2025-02-02 15:41:06 +0000

    Leaflet.js added into project

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "leaflet" # @1.9.4
+# pin "leaflet-providers" # @2.0.0
+pin 'leaflet', to: 'https://ga.jspm.io/npm:leaflet@1.7.1/dist/leaflet-src.js'
+pin 'leaflet-providers', to: 'https://ga.jspm.io/npm:leaflet-providers@1.13.0/leaflet-providers.js'
