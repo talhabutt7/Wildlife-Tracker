add5e4bd8a86bac5f166444e21e454d57511502d
talhabutt7 <talhabuttt@gmail.com>
2025-02-02 15:42:21 +0000

    Map display on the homepage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require leaflet
+import 'leaflet'
+import 'leaflet-providers'
 //= require wildlife_locations
